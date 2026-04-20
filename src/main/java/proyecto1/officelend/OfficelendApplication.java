8cc380ba568e91bde9113af0ed5e4ce2025e1052
xandro2021 <xandrocastro2021@gmail.com>
2026-04-19 21:54:12 -0600

    estructura del proyecto

---
+++ src/main/java/proyecto1/officelend/OfficelendApplication.java
@@ -0,0 +1,13 @@
+package proyecto1.officelend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficelendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficelendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/proyecto1/officelend/OfficelendApplicationTests.java
@@ -0,0 +1,13 @@
+package proyecto1.officelend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficelendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

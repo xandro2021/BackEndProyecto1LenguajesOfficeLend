093fdf41b611e9c3764736607713d65b3c35da2e
xandro2021 <xandrocastro2021@gmail.com>
2026-04-22 14:24:05 -0600

    desactivar la restriccion de CORS

--- src/main/java/proyecto1/officelend/config/CorsConfig.java
+++ src/main/java/proyecto1/officelend/config/CorsConfig.java
@@ -1,28 +1,28 @@
-package proyecto1.officelend.config;
+// package proyecto1.officelend.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.*;
-import org.springframework.web.filter.CorsFilter;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.cors.*;
+// import org.springframework.web.filter.CorsFilter;
 
-import java.util.List;
+// import java.util.List;
 
-@Configuration
-public class CorsConfig {
+// @Configuration
+// public class CorsConfig {
 
-    @Bean
-    public CorsFilter corsFilter() {
-        CorsConfiguration config = new CorsConfiguration();
+//     @Bean
+//     public CorsFilter corsFilter() {
+//         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://127.0.0.1:5500", "http://localhost:5500"));
-        config.setAllowedHeaders(List.of("*"));
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        config.setExposedHeaders(List.of("Authorization"));
+//         config.setAllowCredentials(true);
+//         config.setAllowedOrigins(List.of("http://127.0.0.1:5500", "http://localhost:5500"));
+//         config.setAllowedHeaders(List.of("*"));
+//         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+//         config.setExposedHeaders(List.of("Authorization"));
 
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
+//         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//         source.registerCorsConfiguration("/**", config);
 
-        return new CorsFilter(source);
-    }
-}
+//         return new CorsFilter(source);
+//     }
+// }
